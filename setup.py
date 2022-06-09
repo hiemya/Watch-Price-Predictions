3658313a40d09d958201a226ca6d845327ad4614
Yannick Hiemeleers <yhiemeleers@deloitte.com>
2022-06-09 09:24:05 +0200

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This project is my first data science excerise trying to predict prices of used watches',
+    author='Yannick Hiemeleers',
+    license='MIT',
+)
